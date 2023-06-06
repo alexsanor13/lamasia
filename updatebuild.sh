8fd3d2afc4440814fb22465f11cc17ec7e4ec09b
Alexso47 <alekox97@gmail.com>
2023-06-06 18:51:25 +0200

    build updated

--- updatebuild.sh
+++ updatebuild.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 rm -r ./frontend/build && rm -r ./backend/build && \
 cd frontend && npm run build && \
-cp -r build ../backend/ && \
+cp -r build ../backend/ && echo "build updated" && \
 cd .. && \
 git add . && \
-git commit -m 'build update' && \
-git push origin main
+git commit -m 'build updated' && \
+git push origin main && echo "dev branch updated" && \
+rm -r ../lamasiaevents/* && \
+cp -r ./backend/* ../lamasiaevents/ && \
+rm -r ../lamasiaevents/node_modules && rm ../lamasiaevents/package-lock.json && \
+cd ../lamasiaevents && git add . && git commit -m 'build updated' && git push heroku master && \
+echo "Build update and deploy completed"
 
