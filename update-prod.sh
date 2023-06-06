e95318d4c953ff2656c3af4d34e998db14e99ec1
Alexso47 <alekox97@gmail.com>
2023-06-06 18:55:11 +0200

    build updated

--- update-prod.sh
+++ update-prod.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 rm -r ./frontend/build && rm -r ./backend/build && \
 cd frontend && npm run build && \
-cp -r build ../backend/ && echo "build updated" && \
+cp -r build ../backend/ && echo -e "${GREEN}Build updated${NC}" && \
 cd .. && \
 git add . && \
 git commit -m 'build updated' && \
-git push origin main && echo "dev branch updated" && \
+git push origin main && echo -e "${GREEN}Dev branch updated${NC}" && \
 rm -r ../lamasiaevents/* && \
 cp -r ./backend/* ../lamasiaevents/ && \
 rm -r ../lamasiaevents/node_modules && rm ../lamasiaevents/package-lock.json && \
 cd ../lamasiaevents && git add . && git commit -m 'build updated' && git push heroku master && \
-echo "Build update and deploy completed"
+echo -e "${GREEN}Build update and deploy completed${NC}"
 
