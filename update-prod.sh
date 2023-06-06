bc6022cdd232f6b83f447a9e10fcbed719a9ea2f
Alexso47 <alekox97@gmail.com>
2023-06-06 19:12:00 +0200

    build updated 2023.06.06-19.11.51

--- update-prod.sh
+++ update-prod.sh
@@ -4,15 +4,30 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 TIMESTAMP=$(date "+%Y.%m.%d-%H.%M.%S")
 
-rm -r ./frontend/build && rm -r ./backend/build && \
+if [[ -d "./frontend/build" && -d "./backend/build" ]]; then
+    rm -r ./frontend/build && rm -r ./backend/build
+fi
+
 cd frontend && npm run build && \
 cp -r build ../backend/ && echo -e "${GREEN}Build updated${NC}" && \
 cd .. && \
 git add . && \
 git commit -m "build updated $TIMESTAMP" && \
-git push origin main && echo -e "${GREEN}Dev branch updated${NC}" && \
-rm -r ../lamasiaevents/* && \
-cp -r ./backend/* ../lamasiaevents/ && \
-rm -r ../lamasiaevents/node_modules && rm ../lamasiaevents/package-lock.json && \
+git push origin main && echo -e "${GREEN}Dev branch updated${NC}"
+
+if [[ -d "../lamasiaevents" ]]; then
+    rm -r ../lamasiaevents/*
+fi
+
+cp -r ./backend/* ../lamasiaevents/
+
+if [[ -d "../lamasiaevents/node_modules" ]]; then
+    rm -r ../lamasiaevents/node_modules
+fi
+
+if [[ -f "../lamasiaevents/package-lock.json" ]]; then
+    rm ../lamasiaevents/package-lock.json
+fi
+
 cd ../lamasiaevents && git add . && git commit -m "build updated $TIMESTAMP" && git push heroku master && \
 echo -e "${GREEN}Build update and deploy completed${NC}"
