01233a67ba5b25fabdc2b7e673e688ebe953f429
Alexso47 <alekox97@gmail.com>
2023-06-06 16:28:13 +0200

    git fix

--- backend/app.js
+++ backend/app.js
@@ -38,10 +38,12 @@ if (config.debugMode) {
 app.use('/api/events', eventsRouter)
 app.use('/api/tickets', ticketsRouter)
 
-app.use(express.static(path.join(__dirname, 'build')))
+if (config.debugMode) {
+	app.use(express.static(path.join(__dirname, 'build')))
 
-app.get('*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+	app.get('*', function (req, res) {
+		res.sendFile(path.join(__dirname, 'build', 'index.html'))
+	})
+}
 
 module.exports = app
