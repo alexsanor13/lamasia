489391a8002a66b3e959565cef79cd22cbe9adc1
Alexso47 <alekox97@gmail.com>
2023-06-06 18:44:06 +0200

    build update

--- updatebuild.sh
+++ updatebuild.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-rm -r ./frontend/build && rm -r ./backend/build
-cd frontend && npm run build
-cp -r build ../backend/
-cd ..
-git add .
-git commit -m 'build update'
+rm -r ./frontend/build && rm -r ./backend/build && \
+cd frontend && npm run build && \
+cp -r build ../backend/ && \
+cd .. && \
+git add . && \
+git commit -m 'build update' && \
 git push origin main
+
