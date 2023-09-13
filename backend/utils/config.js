b3ef6b4a4171cd83d9302bb196fe5b6e6187479f
Alexso47 <alekox97@gmail.com>
2023-09-13 05:26:56 +0200

    build updated 2023.09.13-05.26.49

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -12,7 +12,7 @@ const rateLimit = require('express-rate-limit')
 
 const apiLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 50, // limit each IP to 100 requests per windowMs
+	max: 1000, // limit each IP to 100 requests per windowMs
 	message: 'Too many requests from this IP, please try again later',
 })
 
