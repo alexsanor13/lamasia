80375d587f6460e4c75f8243386abbd8db7d3281
Alexso47 <alekox97@gmail.com>
2023-09-21 01:51:39 +0200

    build updated 2023.09.21-01.51.31

--- frontend/src/components/EventBox.js
+++ frontend/src/components/EventBox.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './EventBox.css'
 import utils from '../common/utils'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import aFuegoSVG from '../assets/placeholders/aFuego.svg'
+import fuckCovidSVG from '../assets/placeholders/fuckCovid.svg'
+import genesisSVG from '../assets/placeholders/genesis.svg'
+import pandoraSVG from '../assets/placeholders/pandora.svg'
+import pecadosSVG from '../assets/placeholders/pecados.svg'
+import revolutionSVG from '../assets/placeholders/revolution.svg'
 
 const EventBox = ({ event }) => {
 	const { title, date, image } = event
@@ -32,10 +39,36 @@ const EventBox = ({ event }) => {
 		  }
 		: { containerClass: 'event-box', dateClass: 'event-date' }
 
+	const placeholder = (title) => {
+		switch (title) {
+			case 'PANDORA':
+				return pandoraSVG
+			case 'GÉNESIS':
+				return genesisSVG
+			case 'TOR7UGA':
+				return pecadosSVG
+			case 'R3VOLUTION':
+				return revolutionSVG
+			case 'A FUEG🔥':
+				return aFuegoSVG
+			case 'F☠️CK COVID':
+				return fuckCovidSVG
+			default:
+				return fuckCovidSVG
+		}
+	}
+
 	return (
 		<div className={eventClass.containerClass}>
 			<div className="event-image-container">
-				<img src={image} alt={title} className="event-image" />
+				{/* <img src={image} alt={title} className="event-image" /> */}
+				<LazyLoadImage
+					src={image}
+					placeholderSrc={placeholder()}
+					alt={title}
+					effect="blur"
+					className="event-image"
+				/>
 			</div>
 			<div className="event-info-container outlined-text">
 				<p className={eventClass.dateClass}>{utils.getDDMMYYYDate(date)}</p>
