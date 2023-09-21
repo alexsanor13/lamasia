6606ca050ce21219469d1b9df072ad0e9d727a3c
Alexso47 <alekox97@gmail.com>
2023-09-21 02:08:44 +0200

    build updated 2023.09.21-02.08.36

--- frontend/src/components/EventBox.js
+++ frontend/src/components/EventBox.js
@@ -2,13 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './EventBox.css'
 import utils from '../common/utils'
+
 import { LazyLoadImage } from 'react-lazy-load-image-component'
-import aFuegoSVG from '../assets/placeholders/aFuego.svg'
 import fuckCovidSVG from '../assets/placeholders/fuckCovid.svg'
+import aFuegoSVG from '../assets/placeholders/aFuego.svg'
 import genesisSVG from '../assets/placeholders/genesis.svg'
 import pandoraSVG from '../assets/placeholders/pandora.svg'
 import pecadosSVG from '../assets/placeholders/pecados.svg'
 import revolutionSVG from '../assets/placeholders/revolution.svg'
+
+const placeholderDictionary = {
+	PANDORA: pandoraSVG,
+	GÉNESIS: genesisSVG,
+	TOR7UGA: pecadosSVG,
+	R3VOLUTION: revolutionSVG,
+	'A FUEG🔥': aFuegoSVG,
+	'F☠️CK COVID': fuckCovidSVG,
+	default: fuckCovidSVG,
+}
 
 const EventBox = ({ event }) => {
 	const { title, date, image } = event
@@ -39,32 +50,15 @@ const EventBox = ({ event }) => {
 		  }
 		: { containerClass: 'event-box', dateClass: 'event-date' }
 
-	const placeholder = (title) => {
-		switch (title) {
-			case 'PANDORA':
-				return pandoraSVG
-			case 'GÉNESIS':
-				return genesisSVG
-			case 'TOR7UGA':
-				return pecadosSVG
-			case 'R3VOLUTION':
-				return revolutionSVG
-			case 'A FUEG🔥':
-				return aFuegoSVG
-			case 'F☠️CK COVID':
-				return fuckCovidSVG
-			default:
-				return fuckCovidSVG
-		}
-	}
-
 	return (
 		<div className={eventClass.containerClass}>
 			<div className="event-image-container">
 				{/* <img src={image} alt={title} className="event-image" /> */}
 				<LazyLoadImage
 					src={image}
-					placeholderSrc={placeholder()}
+					placeholderSrc={() =>
+						placeholderDictionary[title] || placeholderDictionary['default']
+					}
 					alt={title}
 					effect="blur"
 					className="event-image"
--- frontend/src/components/Soon.js
+++ frontend/src/components/Soon.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import soon from '../assets/gifs/soon.gif'
 import './Soon.css'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import placeholder from '../assets/placeholders/fuckCovid.svg'
 
 const Soon = () => {
 	return (
 		<section>
 			<div className="soon-container">
 				<span>soon...</span>
-				<img src={soon} alt="Soon." />
+				<LazyLoadImage
+					src={soon}
+					placeholderSrc={placeholder}
+					alt="Soon."
+					effect="blur"
+				/>
 			</div>
 		</section>
 	)
