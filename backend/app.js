55720f5c5bee9e408b8bc58aafc9dc3bf0779fb5
alexsanor13 <alexsanor13@gmail.com>
2024-03-15 23:23:19 +0100

    build updated 2024.03.15-23.23.17

--- backend/app.js
+++ backend/app.js
@@ -54,12 +54,10 @@ app.use('/api/tickets', ticketsRouter)
 app.use('/api/scanner', scannerRouter)
 app.use('/api/transactions', transactionsRouter)
 
-if (!config.debugMode) {
-	app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'dist')))
 
-	app.get('*', function (req, res) {
-		res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-	})
-}
+app.get('*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
 
 module.exports = app
