68bc5a4ff9516351a0c37d31f01bcaca85d961dc
alexsanor13 <alexsanor13@gmail.com>
2024-03-16 00:01:24 +0100

    build updated 2024.03.16-00.01.22

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,5 @@ import svgr from 'vite-plugin-svgr'
 export default defineConfig({
 	plugins: [svgr(), react()],
 	publicDir: '/public',
-	build: {
-		// Exclude images for minification
-		assetsInclude: ['favicon.ico', 'logo.png', 'scanner-beep.mp3'],
-	},
+	copyPublicDir: false,
 })
